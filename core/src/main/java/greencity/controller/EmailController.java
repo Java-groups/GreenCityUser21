01d1912da31cfa53fbe0fb3fa14c9f30da0b86de
K4sik <kasiks597@gmail.com>
2024-05-23 19:23:35 +0300

    212 Code cleanup for EmailController

--- core/src/main/java/greencity/controller/EmailController.java
+++ core/src/main/java/greencity/controller/EmailController.java
@@ -12,17 +12,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/email")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class EmailController {
-    @Autowired
+
     private final EmailService emailService;
 
     /**
