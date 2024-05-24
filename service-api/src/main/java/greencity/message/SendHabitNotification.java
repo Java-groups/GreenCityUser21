d3b562566962419aafea209af9dbdb0fe70ae4bf
K4sik <kasiks597@gmail.com>
2024-05-24 14:49:15 +0300

    Fix: 215 - handle 400 Bad Request for sendHabitNotification endpoint

--- service-api/src/main/java/greencity/message/SendHabitNotification.java
+++ service-api/src/main/java/greencity/message/SendHabitNotification.java
@@ -1,6 +1,10 @@
 package greencity.message;
 
 import java.io.Serializable;
+
+import greencity.constant.ValidationConstants;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,5 +19,8 @@ import lombok.ToString;
 @NoArgsConstructor
 public class SendHabitNotification implements Serializable {
     private String name;
+
+    @NotBlank
+    @Email(message = ValidationConstants.INVALID_EMAIL)
     private String email;
 }
