715dbcf977c45ee2ed6e351b0f28327500d5fc18
IliaWithHat <rozhkoilia5533@gmail.com>
2024-05-27 14:49:38 +0300

    [264] add 404 response code

--- core/src/main/java/greencity/controller/UserController.java
+++ core/src/main/java/greencity/controller/UserController.java
@@ -481,7 +481,10 @@ public class UserController {
      * @author Orest Mamchuk
      */
     @Operation(summary = "update via UserManagement")
-    @ApiResponses(value = @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED))
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "401", description = HttpStatuses.UNAUTHORIZED),
+        @ApiResponse(responseCode = "404", description = HttpStatuses.NOT_FOUND)
+    })
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void updateUserManagement(
