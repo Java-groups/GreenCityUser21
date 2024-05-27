612f149d7919a93416309bd36c8de995325e860d
Viktoriia Herchanivska <viktoriia.herchanivska@gmail.com>
2024-05-27 22:17:07 +0300

    357 fix bug for PATCH /user/deleteProfilePicture unauthorized add test case

--- core/src/test/java/greencity/controller/UserControllerWithSecurityConfigTest.java
+++ core/src/test/java/greencity/controller/UserControllerWithSecurityConfigTest.java
@@ -12,8 +12,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -23,11 +27,16 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.security.Principal;
+import java.util.Collections;
+
+import static greencity.enums.Role.ROLE_ADMIN;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -40,6 +49,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         "refreshTokenValidTimeInMinutes=1440",
         "tokenKey=secretTokenKey"
 })
+@ActiveProfiles("dev")
 public class UserControllerWithSecurityConfigTest {
     private static final String userLink = "/user";
 
@@ -84,5 +94,12 @@ public class UserControllerWithSecurityConfigTest {
                         .with(anonymous()))
                 .andExpect(status().isUnauthorized());
         verify(userService, times(0)).getEmailNotificationsStatuses();
+    }
+
+    @Test
+    @WithMockUser(username = "testmail@mail.com", roles = "ADMIN")
+    void deleteUserProfilePicture_isOk() throws Exception {
+        mockMvc.perform(patch(userLink + "/deleteProfilePicture"))
+                .andExpect(status().isOk());
     }
 }
