2fa0b7dedc7e17d89b0f90c6a4b808b6b19677c2
K4sik <kasiks597@gmail.com>
2024-05-27 11:18:08 +0300

    Added a few little validation to SendChangePlaceStatusEmailMessage

--- service-api/src/main/java/greencity/message/SendChangePlaceStatusEmailMessage.java
+++ service-api/src/main/java/greencity/message/SendChangePlaceStatusEmailMessage.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import greencity.constant.ValidationConstants;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,11 +17,18 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public final class SendChangePlaceStatusEmailMessage implements Serializable {
+    @NotBlank
+    @Pattern(regexp = ValidationConstants.USERNAME_REGEXP, message = ValidationConstants.USERNAME_MESSAGE)
     private String authorFirstName;
+
+    @NotBlank
+    @Size(max = ValidationConstants.PLACE_NAME_MAX_LENGTH, message = "Place Name should contain maximum {max} symbols")
     private String placeName;
+
+    @NotBlank
     private String placeStatus;
 
     @NotBlank
-    @Email(message = ValidationConstants.INVALID_EMAIL)
+    @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = ValidationConstants.INVALID_EMAIL)
     private String authorEmail;
 }
